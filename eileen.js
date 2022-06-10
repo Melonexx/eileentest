acdbad141e49a0ddce33e85c0521868ccb3f4373
Robert Hasemann <robert.hasemann@concordia.de>
2022-06-10 21:58:15 +0200

    accessing properties und delete

--- eileen.js
+++ eileen.js
@@ -1,26 +1,12 @@
-let list = [2, -3];
-console.log(list);
+let o = { name: "ida", 4: true };
+console.log(o[2 + 2]);
+console.log(o[4]);
+console.log(o[-3]);
 
-for (let el of list) {
-  el = el + 1;
-  console.log(el);
-}
-
-console.log(list);
-
-function plus(a) {
-  for (let i = 0; i < a.length; i = i + 1) {
-    a[i] = a[i] + 1;
-  }
-}
-plus(list);
-console.log(list);
-
-function langi(a) {
-  let list5 = [];
-  for (let i = 0; i <= a; i++) {
-    list5.push(i);
-  }
-  return list5;
-}
-console.log(langi(5));
+let ob = {};
+ob.name = "eileen";
+console.log(ob);
+ob.age = 12;
+console.log(ob);
+delete ob.age;
+console.log(ob);
