1eaebe1d0ed1b71957a07ec9488a602ceaba1744
Robert Hasemann <robert.hasemann@concordia.de>
2022-06-13 20:05:51 +0200

    function exercises (function, return, testing)

--- eileen.js
+++ eileen.js
@@ -1,29 +1,35 @@
-// if else
-if ("hugo") {
-  console.log("hai");
-} else {
-  console.log("schü");
+//  function die den wert true zurück gibt
+
+/** gibt true zurück */
+function wahr() {
+  return true;
+}
+console.log(wahr() === true);
+
+// function, welche zwei zahlen multipliziert
+
+/** multipliziert zwei zahlen */
+function product(a, b) {
+  return a * b;
+}
+console.log(product(-2, -2) === 4);
+console.log(product(2, 2) === 4);
+console.log(product(-2, 2) === -4);
+console.log(product(2, -2) === -4);
+console.log(product(0, -2) === 0);
+console.log(product(2, 0) === 0);
+
+// function, welche prüft ob eine zahl negativ ist
+
+/**prüft ob zahl negativ ist */
+function negativ(a) {
+  if (a < 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-if (NaN) {
-  console.log("hai");
-} else {
-  console.log("schü");
-}
-if (1) {
-  console.log("hai");
-} else {
-  console.log("schü");
-}
-if (null) {
-  console.log("hai");
-} else {
-  console.log("schü");
-}
-
-if (0 == null) {
-  //achtung
-  console.log("hai");
-} else {
-  console.log("schü");
-}
+console.log(negativ(2) === false);
+console.log(negativ(-2) === true);
+console.log(negativ(0) === false);
