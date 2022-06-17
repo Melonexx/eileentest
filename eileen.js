d61da41484d29540c39905b9cd4c30c49f33645b
Robert Hasemann <robert.hasemann@concordia.de>
2022-06-17 21:19:34 +0200

    lists und while loop

--- eileen.js
+++ eileen.js
@@ -1,48 +1,19 @@
-//  function die den wert true zurück gibt
-
-/** gibt true zurück */
-function wahr() {
-  return true;
-}
-console.log(wahr() === true);
-
-// function, welche zwei zahlen multipliziert
-
-/** multipliziert zwei zahlen */
-function product(a, b) {
-  return a * b;
-}
-console.log(product(-2, -2) === 4);
-console.log(product(2, 2) === 4);
-console.log(product(-2, 2) === -4);
-console.log(product(2, -2) === -4);
-console.log(product(0, -2) === 0);
-console.log(product(2, 0) === 0);
-
-// function, welche prüft ob eine zahl negativ ist
-
-/**prüft ob zahl negativ ist */
-function negativ(a) {
-  console.log("hai");
-  if (a < 0) {
-    return true;
-  } else {
-    return false;
-  }
+let a = 0;
+while (a < 10) {
+  a++;
+  console.log(a);
 }
 
-console.log(negativ(2) === false);
-console.log(negativ(-2) === true);
-console.log(negativ(0) === false);
+let list = ["apfel", "börne"];
+console.log(list);
+list.push("banane");
+console.log(list);
 
-// function negativ mit weniger zeichen schreiben
+a = 0;
+console.log(a);
 
-function negative(a) {
-  //wenn man functionen gleich benennt, werden sie überschrieben
-  console.log("haii");
-  return a < 0;
+while (a < list.length) {
+  console.log(list[a]);
+  a++;
 }
-
-console.log(negative(2) === false);
-console.log(negative(-2) === true);
-console.log(negative(0) === false);
+console.log("hai");
