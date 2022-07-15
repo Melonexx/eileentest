9182734baf927f0fff73b4a4d0b24f4f0d1e8a0d
Robert Hasemann <robert.hasemann@hdi.de>
2022-07-15 17:47:55 +0200

    ES5 class syntax bsp forest

--- eileen.js
+++ eileen.js
@@ -1,40 +1,61 @@
-"use strict";
-console.log(Object.keys({ name: "eileen", age: 22, iq: 136 }));
-console.log(Object.values({ name: "eileen", age: 22, iq: 136 }));
+"use strict"
 
-Math.abs;
-
-console.log(Math.abs(4587 - 22344));
-
-console.log(Math.min(1, 2, 3));
-
-console.log(Math.max(1, 2, 3));
-
-console.log(Math.floor(4.1));
-
-console.log(Math.random());
-
-let a = "eileen";
-console.log(a.substring(0, 3));
-console.log(a.replace("e", "a"));
-console.log(a.replaceAll("e", "a"));
-
-/**replaced beliebig(n mal) oft, beginnend links*/
-function replacentimes(target, searchvalue, replacement, n) {
-  let i = 0;
-  let result = target;
-  while (i < n) {
-    result = result.replace(searchvalue, replacement);
-    i++;
+class Forest {
+  trees;
+  colour;
+  constructor(trees, colour) {
+    this.trees = trees
+    this.colour = colour
   }
-  return result;
+
+  addTree(tree) {
+    this.trees.push(tree)
+  }
+
+  changeColour(newColour) {
+    this.colour = newColour
+  }
 }
-console.log(replacentimes("eileen", "e", "a", 2) === "ailaen");
+class Tree {
+  height;
+  constructor(height) {
+    this.height = height
+  }
+  grow(value) {
+    this.height = this.height + value
+  }
 
-Number.prototype.toExponential;
+}
 
-console.log((1.2345).toFixed(2));
+class NadelBaum extends Tree {
+  constructor(height) {
+    super(height)
+  }
+}
 
-console.log((2000).toExponential());
-console.log((0.02).toExponential());
-//test
+class LaubBaum extends Tree {
+  constructor(height) {
+    super(height)
+  }
+}
+
+const baum1 = new Tree(6)
+const baum2 = new Tree(7)
+const nadelBaum1 = new NadelBaum(8)
+const laubBaum1 = new LaubBaum(4)
+
+const trees = [baum1, baum2, nadelBaum1, laubBaum1]
+const wald1 = new Forest(trees, "green")
+
+
+
+
+console.log(wald1)
+
+wald1.changeColour("yellow")
+
+console.log(wald1)
+
+baum1.grow(16)
+
+console.log(baum1)
