33d30ef5f47de9a216f24628b9a1a2d54cb1a4f1
Robert Hasemann <robert.hasemann@hdi.de>
2022-07-22 18:48:12 +0200

    dom klick-anzahl-zähler

--- eileen.js
+++ eileen.js
@@ -4,8 +4,8 @@ console.log(window.document)
 
 
 
-
-let queryselectordomeins = document.querySelector("#eins")
+const x = "#eins";
+let queryselectordomeins = document.querySelector(x)
 
 const paragraphtogglebutton = document.querySelector("#paragraphtogglebutton")
 
@@ -15,3 +15,19 @@ paragraphtogglebutton.addEventListener("click", function (event) {
   console.log(event)
   queryselectordomeins.classList.toggle("red")
 })
+
+const clickanzahl = document.querySelector("#clickizähler")
+
+
+
+function clickzahl() {
+  counter++;
+  console.log(counter)
+}
+
+
+
+/**zeigt anzahl wie häufig der button geclicked wurde in console an */
+clickanzahl.addEventListener("click", clickzahl)
+
+let counter = 0 
