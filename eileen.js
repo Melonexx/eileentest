6406ad2958810cbf3297dbc7203a846aed25824a
Robert Hasemann <robert.hasemann@hdi.de>
2022-07-29 22:20:34 +0200

    theme changer toggling two classes

--- eileen.js
+++ eileen.js
@@ -1,14 +1,26 @@
 "use strict"
 //was ist der unterschied zwischen queryselector und getelementbyid??
-let changedTheme = document.querySelector("#changedTheme")
+let rootElement = document.querySelector("#root")
 let changingButton = document.getElementById("changeButton")
 
 const changeButton = document.querySelector("#changeButton")
 
+const classBluei = "bluei";
+const classRosai = "rosai";
 
-changeButton.addEventListener("click",function(){
-changedTheme.classList.toggle("bluei")
-changingButton.innerHTML="reset"
+changeButton.addEventListener("click", function () {
+
+  if (rootElement.classList.contains(classBluei)) {
+    rootElement.classList.add(classRosai);
+    rootElement.classList.remove(classBluei);
+    changeButton.innerHTML = "change to blue";
+  }
+  else {
+    rootElement.classList.remove(classRosai);
+    rootElement.classList.add(classBluei);
+    changeButton.innerHTML = "change to rosa";
+  }
+
 })
 
 
