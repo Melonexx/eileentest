2c8ed234d8d6918885fff16b9c96fb4e8f7e55a7
Robert Hasemann <robert.hasemann@hdi.de>
2022-07-29 22:49:17 +0200

    refactored theme toggle

--- eileen.js
+++ eileen.js
@@ -7,19 +7,27 @@ const changeButton = document.querySelector("#changeButton")
 
 const classBluei = "bluei";
 const classRosai = "rosai";
+const rosaText = "change to blue";
+const blueText = "change to rosa";
+const localStorageThemeKey = "theme"; //alles (variablen und functions) was man mehr als 1 mal schreibt, sollte man auslagern.
+
+const storedThemeClass = localStorage.getItem(localStorageThemeKey) ?? classRosai;
+rootElement.classList.add(storedThemeClass)
+changeButton.innerHTML = storedThemeClass === classRosai ? rosaText : blueText;
+
+
+function toggleTheme(oldClass, newClass, newText) {
+  rootElement.classList.add(newClass);
+  rootElement.classList.remove(oldClass);
+  changeButton.innerHTML = newText;
+  localStorage.setItem(localStorageThemeKey, newClass);
+}
 
 changeButton.addEventListener("click", function () {
 
-  if (rootElement.classList.contains(classBluei)) {
-    rootElement.classList.add(classRosai);
-    rootElement.classList.remove(classBluei);
-    changeButton.innerHTML = "change to blue";
-  }
-  else {
-    rootElement.classList.remove(classRosai);
-    rootElement.classList.add(classBluei);
-    changeButton.innerHTML = "change to rosa";
-  }
+  if (rootElement.classList.contains(classBluei))
+    toggleTheme(classBluei, classRosai, rosaText)
+  else toggleTheme(classRosai, classBluei, blueText)
 
 })
 
